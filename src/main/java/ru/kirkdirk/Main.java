9db96e782e3569360fc3c633670ea190b5c06946
KirkDirk <jackalex@ya.ru>
2024-11-29 20:39:47 +0500

    Add final

--- src/main/java/ru/kirkdirk/Main.java
+++ src/main/java/ru/kirkdirk/Main.java
@@ -1,5 +1,7 @@
 package ru.kirkdirk;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 import java.util.*;
 
 public class Main {
@@ -19,6 +21,9 @@ public class Main {
      * Мапа для результатов розыгрыша c заменой первоначального выбора
      */
     static Map<Integer, String> resultsSelectionChange;
+    /**
+     * Мапа для результата без замены первоначального выбора
+      */
     static Map<Integer, String> resultsWithoutChaging;
 
     public static void main(String[] args) {
@@ -28,6 +33,8 @@ public class Main {
         random = new Random();
         resultsSelectionChange = new HashMap<>();
         resultsWithoutChaging = new HashMap<>();
+
+        double [] array = new double[raffle]; // для проверки common.math
 
         System.out.println("\n Парадокс Монти Холла: " +
                 countDoors + " двери и " +
@@ -48,12 +55,16 @@ public class Main {
                     openDoor = i;
                 }
             }
-//          Определяем дверь второго выбора
+
+            array[round] = openDoor;// для проверки common.math
+
+//          Определяем дверь второго выбора при смене первоначального выбора
             for (int i = 0; i < countDoors; i++) {
                 if (i != openDoor && i != choice1Door) {
                     choice2Door = i;
                 }
             }
+            // Проверяем и записываем в HashMap'ы результаты раунда
             if (winDoor == choice2Door) {
                 resultsSelectionChange.put(round, "win");
             } else {
@@ -65,13 +76,23 @@ public class Main {
                 resultsWithoutChaging.put(round, "lose");
             }
         }
-        int countWinSC = Collections.frequency(new ArrayList<String>(resultsSelectionChange.values()), "win");
-        System.out.println("Количество win при смене выбора: " + countWinSC);
 
-        int countWinWC = Collections.frequency(new ArrayList<String>(resultsWithoutChaging.values()), "win");
-        System.out.println("Количество win без смены выбора: " + countWinWC);
+        DescriptiveStatistics ds = new DescriptiveStatistics(array);
+        System.out.println("Сумма элементов array = " + ds.getSum());
 
+        // Выводим результаты розыгрыша в 1000 раундов
+        int countWinSC = Collections.frequency(new ArrayList<String>(
+                resultsSelectionChange.values()), "win");
+        int countLoseSC = Collections.frequency(new ArrayList<String>(
+                resultsSelectionChange.values()), "lose");
+        System.out.println("\nПри смене первоначального выбора: \n Количество win: "
+                + countWinSC + "\n Количество lose: " + countLoseSC);
+
+        int countWinWC = Collections.frequency(new ArrayList<String>(
+                resultsWithoutChaging.values()), "win");
+        int countLoseWC = Collections.frequency(new ArrayList<String>(
+                resultsWithoutChaging.values()), "lose");
+        System.out.println("\nБез смены первоначального выбора: \n Количество win: "
+                + countWinWC + "\n Количество lose: " + countLoseWC);
     }
-
-
 }
